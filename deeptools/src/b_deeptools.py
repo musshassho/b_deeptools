8556267114fcad20f80883d831960fc48e1c5b00
Boris Martinez Castillo <bmartinezcastillo@mtl-rent-26.atomicfiction.net>
2019-05-27 15:34:39 -0400

    my first commit

---
+++ deeptools/src/b_deeptools.py
@@ -0,0 +1,24 @@
+#######################################################################################################################
+
+__author__ = "Boris Martinez Castillo"
+__version__ = "1.0.1"
+__maintainer__ = "Boris Martinez Castillo"
+__email__ = "boris.vfx@outlook.com"
+
+########################################################################################################################
+
+## TO DO
+
+## CREATE DEEP HOLDOUT SETUPS
+## CREATE SUPERPASS SETUP
+## CREATE AUTO CROP SETUP
+## ...
+
+#IMPORTS
+
+import nuke
+import nukescripts
+import itertools
+
+#DEFINITIONS
+
