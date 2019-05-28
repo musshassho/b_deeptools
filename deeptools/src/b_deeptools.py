ad9a90c279d9a64b1516cd72a96647e4a5683a4d
Boris Martinez Castillo <bmartinezcastillo@mtl-rent-26.atomicfiction.net>
2019-05-28 16:46:20 -0400

    updating b_deeptools code

--- deeptools/src/b_deeptools.py
+++ deeptools/src/b_deeptools.py
@@ -1,24 +1,119 @@
-#######################################################################################################################
 
-__author__ = "Boris Martinez Castillo"
-__version__ = "1.0.1"
-__maintainer__ = "Boris Martinez Castillo"
-__email__ = "boris.vfx@outlook.com"
 
-########################################################################################################################
+def select_node(node_class):
 
-## TO DO
+    class_ = node_class
 
-## CREATE DEEP HOLDOUT SETUPS
-## CREATE SUPERPASS SETUP
-## CREATE AUTO CROP SETUP
-## ...
+    try:
+        node = nuke.selectedNode()
+        if node.Class() == class_:
+            node['selected'].setValue(False)
+            return node
+        else:
+            message = "Please, select a {} Node".format(class_)
+            nuke.message(message)
 
-#IMPORTS
+    except ValueError:
+        message = "Please, select a {} Node".format(class_)
+        nuke.message(message)
 
-import nuke
-import nukescripts
-import itertools
 
-#DEFINITIONS
+def find_dependencies(node_class):
+
+    node = select_node(node_class)
+    dep_node = nuke.dependencies(node)
+    
+    for node in dep_node:
+        print node.name()
+    
+    return dep_node
+
+
+def get_node_position(node):
+    
+    pos_dict = {"x_pos": node.xpos(),"y_pos": node.ypos()} 
+    
+    return pos_dict
+
+
+def create_node_with_position(nodename,connect_node,x=0,y=0):
+
+     node = nuke.createNode(nodename)
+     node['selected'].setValue(False)
+
+     node.setXpos(x)
+     node.setYpos(y)
+
+     node.setInput(0,connect_node)
+
+     return node
+
+
+def create_deep_holdout_setup(node_class):
+
+    dependencies = find_dependencies(node_class)
+
+    for node in dependencies:
+      
+        pos = get_node_position(node)
+        create_node_with_position("Dot",node,pos["x_pos"],pos["x_pos"])
+
+create_deep_holdout_setup("DeepRecolor")
+def select_node(node_class):
+
+    class_ = node_class
+
+    try:
+        node = nuke.selectedNode()
+        if node.Class() == class_:
+            node['selected'].setValue(False)
+            return node
+        else:
+            message = "Please, select a {} Node".format(class_)
+            nuke.message(message)
+
+    except ValueError:
+        message = "Please, select a {} Node".format(class_)
+        nuke.message(message)
+
+
+def find_dependencies(node_class):
+
+    node = select_node(node_class)
+    dep_node = nuke.dependencies(node)
+    
+    for node in dep_node:
+        print node.name()
+    
+    return dep_node
+
+
+def get_node_position(node):
+    
+    pos_dict = {"x_pos": node.xpos(),"y_pos": node.ypos()} 
+    
+    return pos_dict
+
+
+def create_node_with_position(nodename,connect_node,x=0,y=0):
+
+     node = nuke.createNode(nodename)
+     node['selected'].setValue(False)
+
+     node.setXpos(x)
+     node.setYpos(y)
+
+     node.setInput(0,connect_node)
+
+     return node
+
+    
+def create_deep_holdout_setup(node_class):
+
+    dependencies = find_dependencies(node_class)
+
+    for node in dependencies:
+      
+        pos = get_node_position(node)
+        create_node_with_position("Dot",node,pos["x_pos"],pos["x_pos"])
 
